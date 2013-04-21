b1df99d31b33c9e206b887c800f3db056b518916
John Scheible <johnscheible@gmail.com>
2013-04-20 23:45:25 -0400

    Added Bouncer policy

--- SpewDP/src/com/johnscheible/spewdp/WifiChangeReceiver.java
+++ SpewDP/src/com/johnscheible/spewdp/WifiChangeReceiver.java
@@ -1,5 +1,10 @@
 package com.johnscheible.spewdp;
 
+import java.io.IOException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -29,6 +34,7 @@ public class WifiChangeReceiver extends BroadcastReceiver {
 			                   "Connected to " + netInfo.toString(),
 			                   Toast.LENGTH_LONG).show();
 				context.startService(service);
+				new Thread(new BouncerRunner(netInfo, context)).start();
 				break;
 			case DISCONNECTING:
 			case DISCONNECTED:
@@ -36,6 +42,63 @@ public class WifiChangeReceiver extends BroadcastReceiver {
 				context.stopService(service);
 		}
 		
+	}
+	
+	public class BouncerRunner implements Runnable {
+		private final Context mContext;
+		
+		public BouncerRunner(NetworkInfo netInfo, Context context) {
+			mContext = context;
+		}
+		
+		@Override
+		public void run() {
+			final int NUM_TRIES = 2;
+			
+			for (int i = 0; i < NUM_TRIES; i++) {
+				// Ping Google to make sure we're online
+				try {
+					HttpsURLConnection urlc = 
+							(HttpsURLConnection) (new URL("http://www.google.com").openConnection());
+					urlc.setRequestProperty("User-Agent", "Test");
+					urlc.setRequestProperty("Connection", "close");
+					urlc.setConnectTimeout(1500);
+					urlc.connect();
+					if (urlc.getResponseCode() != 200) {
+						Log.i(TAG, "Could not reach Google on attempt " + i);
+						bounce(mContext);
+						break;
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "Error in checking Google on attmept " + i);
+					bounce(mContext);
+					break;
+				}
+				
+				// Sleep 3 seconds between pings
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					Log.e(TAG, "Interrupted in sleep between pings");
+				}
+			}
+			
+			Log.i(TAG, "WiFi connection is OK: You may proceed...");
+		}
+		
+		private void bounce(Context context) {
+			WifiManager wfm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+			Log.i(TAG, "Attempting to disconnect");
+	    	Log.i(TAG, "WiFi status: " + wfm.getWifiState());
+	    	// If we're not connected to WiFi, let user know we can't disconnect
+	    	if (wfm.getConnectionInfo() == null) {
+	    		Log.d(TAG, "Tried to bounce but wasn't connected to WiFi");
+	    	} else {
+	    		if (!wfm.disconnect()) {
+	    			Log.d(TAG, "Bounce failed to disconnect from WiFi");
+	    		}
+	    	}
+		}
 	}
 
 }
